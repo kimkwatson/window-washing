78d4edd28694fa318adfab713f7510ac7e15ceb1
kimkwatson <kimwatson731@gmail.com>
2026-03-12 15:56:14 -0700

    added forms.py

---
+++ services/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+
+
+class ServiceRequestForm(forms.Form):
+    fname = forms.CharField(label="First name", max_length=50)
+    lname = forms.CharField(label="Last name", max_length=50)
+    email = forms.EmailField(label="Email")
+    phone = forms.CharField(label="Phone", max_length=12)
+
+    address = forms.CharField(label="Street address", max_length=100)
+    city = forms.CharField(label="City", max_length=50)
+    state = forms.CharField(label="State", max_length=20)
+    zip = forms.CharField(label="ZIP code", max_length=10)
+
+    windows = forms.IntegerField(label="Number of windows", min_value=5, max_value=50)
+
+    SERVICE_CHOICES = [
+        ("inside", "Inside"),
+        ("outside", "Outside"),
+        ("both", "Inside & Outside"),
+    ]
+
+    service = forms.ChoiceField(choices=SERVICE_CHOICES, widget=forms.RadioSelect)
+
+    day = forms.DateField(label="Preferred date")
+
+    notes = forms.CharField(
+        label="Notes",
+        required=False,
+        widget=forms.Textarea
+    )
--- services/views.py
+++ services/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .forms import ServiceRequestForm
 
 def home(request):
     return render(request, 'services/home.html')
 
 def request_service(request):
     if request.method == 'POST':
-        name = request.POST.get('fname')
-        return render(request, 'services/confirmation.html', {'name': name})
-    
-    return render(request, 'services/request_service.html')
+        form = ServiceRequestForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            return render(request, 'services/confirmation.html', {'data': data})
+    else:
+        form = ServiceRequestForm
+
+    return render(request, 'services/request_service.html', {'form': form})

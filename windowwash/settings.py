d72e4d99519021171de541238d0230a3e62a5c4c
kimkwatson <kimwatson731@gmail.com>
2026-03-21 20:20:59 -0700

    configured settings for deployment to render

--- windowwash/settings.py
+++ windowwash/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(4+-c6x*snv@0+7$9!xh37!f1#($6@-tl4!_&mq1t3r-w^&1k1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

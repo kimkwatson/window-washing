af0c864d1a71f506df3260446c62d23208919391
kimkwatson <kimwatson731@gmail.com>
2026-03-21 22:51:00 -0700

    added comments to view.py

--- services/models.py
+++ services/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
 class ServiceRequest(models.Model):
+
+    # customer info
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=12)
 
+    # address info
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=20)
     zip = models.CharField(max_length=10)
 
+    # job details
     stories = models.IntegerField()
-
     windows = models.IntegerField()
 
+    # valid service types in database
     SERVICE_CHOICES = [
         ("inside", "Inside"),
         ("outside", "Outside"),
@@ -27,11 +31,13 @@ class ServiceRequest(models.Model):
 
     notes = models.TextField(blank=True)
     
+    # admin tracking fields
     completed = models.BooleanField(default=False)
-    
     paid = models.BooleanField(default=False)
 
+    # auto set date when request is created
     created_at = models.DateTimeField(auto_now_add=True)
 
+    # how object is displayed in admin interface
     def __str__(self):
         return f"{self.fname} {self.lname} - {self.day}"
--- services/views.py
+++ services/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from .forms import ServiceRequestForm
 from .models import ServiceRequest
 
+# render homepage
 def home(request):
     return render(request, 'services/home.html')
 
+# handle displaying the form and processing submissions
 def request_service(request):
+    
+    # if the form is submitted
     if request.method == 'POST':
         form = ServiceRequestForm(request.POST)
 
+        # validate form data
         if form.is_valid():
-            data = form.cleaned_data
+            data = form.cleaned_data # cleaned and validated input
 
+            # create and save a new ServiceRequest in the database
             saved_request = ServiceRequest.objects.create(
                 fname=data['fname'],
                 lname=data['lname'],
@@ -28,8 +34,11 @@ def request_service(request):
                 notes=data['notes'],
             )
             
+            # show confirmation page with saved data
             return render(request, 'services/confirmation.html', {'data': saved_request})
     else:
+        # if not submitted yet, display an empty form
         form = ServiceRequestForm()
 
+    # render the form page (either first load or with errors)
     return render(request, 'services/request_service.html', {'form': form})
